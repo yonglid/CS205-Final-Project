ae6e1355645a5d2b87da23c02e6e1ba1f8440e62
yonglid <ydich@college.harvard.edu>
2017-04-10 00:01:43 -0400

    goes through each line in the text file now and plots all those points in the png

--- visualization.py
+++ visualization.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt 
 import math 
 import numpy as np 
+import time
 
 #Constants
 L = 3.0
 N = 300 
+spacestep = L/float(N)
 BCL = 400.0 
 sd = 20.0 
 timestep = 10**(-2)
@@ -18,22 +20,44 @@ resolution = 600
 
 
 
-def file_to_png(f): 
+def file_to_png(): 
 
 	print "Initializing..."
 
 	T = int(T_output/timestep)
 	frames = math.floor(T/resolution)
 	#setting y and x limits of the current axes (ylim( (ymin, ymax) ))
-	plt.ylim(0, 1)
-	plt.xlim(0, L)
-	plt.xlabel('res:' + str(resolution) + ', L: ' + str(L) + ', N: ' + str(N) + ', BCL: ' + str(BCL) + ', sd: ' + str(sd))
+	ary1 = []
+	for i in range(0,N+1):
+		ary1.append(i*spacestep)
+
+	# print ary1
+	
 	# for testing, will need to change fulldatac.txt to fulldatac 
 	# later try putting "f" in the function argument and change code to file = open("%s" % f, "r")
-	file = open("%s" % f, "r")
-	line = file.readline()
-	for i, num in enumerate(line.split()): 
-		print num 
+	
+	file = open("%s" % "full_data", "r")
+	# line = file.readline()
+	with file as f: 
+		#iterate through each line
+		#then plot that current line's numbers (they're already a list so just plot that)
+		i = 0 
+		for line in f: 
+			print line 
+			plt.ylim(0, 1)
+			plt.xlim(0, L)
+			plt.xlabel('res:' + str(resolution) + ', L: ' + str(L) + ', N: ' + str(N) + ', BCL: ' + str(BCL) + ', sd: ' + str(sd))
+			plt.plot(ary1, line.split())
+			plt.savefig('data_%d.png'%(i,))
+			plt.gcf().clear()
+			print i,"/",int(frames)
+			i += 1 
+		# for line in f: 
+		# 	numbers = []
+		# 	for i, num in enumerate(line.split()): 
+		# 		numbers.append(num)
+				# print numbers[1]
+
 	#intial time row 
 	# m = 0
 	# print "Loading frame:"
@@ -54,4 +78,6 @@ def file_to_png(f):
 	# 	plt.gcf().clear()
 	# 	print n,"/",int(frames)
 
-file_to_png("full_data") 
+start = time.time()
+file_to_png() 
+print "Time: ", time.time()-start
